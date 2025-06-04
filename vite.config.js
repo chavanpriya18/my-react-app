c610d5976031ede4f4fa238ea309c67a175d1b52
chavanpriya18 <priyankachavan4707@gmail.com>
2025-06-04 15:30:22 +0530

    onelast

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   //  base: '/store-rating-platform/',
-  base: "/my-first-app/",
+  base: "/my-react-app/",
   plugins: [react(), tailwindcss()],
 });
