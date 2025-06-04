23698790a11e1c835bb011592a21ba1eb46cd7f2
chavanpriya18 <priyankachavan4707@gmail.com>
2025-06-04 15:11:19 +0530

    neww added

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   //  base: '/store-rating-platform/',
+  base: "/my-react-app/",
   plugins: [react(),tailwindcss()],
 })
