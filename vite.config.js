442917617efc4b939dd700d820ebe1175fe3a5f9
chavanpriya18 <priyankachavan4707@gmail.com>
2025-06-04 12:23:50 +0530

    addnewtask

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  //  base: '/store-rating-platform/',
   plugins: [react(),tailwindcss()],
 })
