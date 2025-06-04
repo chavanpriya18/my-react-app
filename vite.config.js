3a24048c39412934420ac1d50a5c4ac7f80b3232
chavanpriya18 <priyankachavan4707@gmail.com>
2025-06-04 15:19:31 +0530

    last one

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   //  base: '/store-rating-platform/',
-  base: "/my-react-app/",
-  plugins: [react(),tailwindcss()],
-})
+  base: "/my-app/",
+  plugins: [react(), tailwindcss()],
+});
