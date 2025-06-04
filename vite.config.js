e8aa5f686e20fae6964c91c8170f3d15e164f4d9
chavanpriya18 <priyankachavan4707@gmail.com>
2025-06-04 15:27:14 +0530

    one

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   //  base: '/store-rating-platform/',
-  base: "/my-app/",
+  base: "/my-first-app/",
   plugins: [react(), tailwindcss()],
 });
