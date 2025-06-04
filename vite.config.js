1bffd39010298398a0cf3b13ee3208aa62f33026
chavanpriya18 <priyankachavan4707@gmail.com>
2025-06-04 15:53:19 +0530

    lastrow

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//    base: '/my-react-app/',
+//   plugins: [react(), tailwindcss()],
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-   base: '/my-react-app/',
-  plugins: [react(), tailwindcss()],
+  base: "/my-react-app/",
+  plugins: [react()],
 });
